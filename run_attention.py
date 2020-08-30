9371187acb2c856b62c4e6693551c005a1f6dbd3
Tamar20 <t0583269989@gmail.com>
2020-08-30 11:54:21 +0300

    mobileye_project_part_1

---
+++ run_attention.py
@@ -0,0 +1,124 @@
+import cv2
+
+try:
+    print("Elementary imports: ")
+    import os
+    import json
+    import glob
+    import argparse
+
+    print("numpy/scipy imports:")
+    import numpy as np
+    from scipy import signal as sg
+    import scipy.ndimage as ndimage
+    from scipy.ndimage.filters import maximum_filter
+
+    print("PIL imports:")
+    from PIL import Image
+
+    print("matplotlib imports:")
+    import matplotlib.pyplot as plt
+except ImportError:
+    print("Need to fix the installation")
+    raise
+
+print("All imports okay. Yay!")
+
+
+def rgb2gray(rgb: np.ndarray):
+    return np.dot(rgb[..., :3], [0.2989, 0.5870, 0.1140])
+
+
+def find_tfl_lights(c_image: np.ndarray, **kwargs):
+    """
+    Detect candidates for TFL lights. Use c_image, kwargs and you imagination to implement
+    :param c_image: The image itself as np.uint8, shape of (H, W, 3)
+    :param kwargs: Whatever config you want to pass in here
+    :return: 4-tuple of x_red, y_red, x_green, y_green
+    """
+    kernel = np.array(
+        [[-1, -1, -1, -1, -1],
+         [-1, 8, 8, 8, -1],
+         [-1, 8, 8, 8, -1],
+         [-1, 8, 8, 8, -1],
+         [-1, -1, -1, -1, -1]]) / 9
+    x_red = []
+    y_red = []
+    x_green = []
+    y_green = []
+    red_layer = sg.convolve2d(c_image[:, :, 0], kernel, mode='same', boundary='fill', fillvalue=0)
+    red_layer_filtered = ndimage.maximum_filter(red_layer, 90)
+    red_coordinates = np.argwhere(red_layer_filtered == red_layer)
+    for coordinate in red_coordinates:
+        x_red.append(coordinate[1])
+        y_red.append(coordinate[0])
+    green_layer = sg.convolve2d(c_image[:, :, 1], kernel)
+    green_layer_filtered = ndimage.maximum_filter(green_layer, 100)
+    green_coordinates = np.argwhere(green_layer_filtered == green_layer)
+    for coordinate in green_coordinates:
+        x_green.append(coordinate[1])
+        y_green.append(coordinate[0])
+
+    return x_red, y_red, x_green, y_green
+
+
+def show_image_and_gt(image, objs, fig_num=None):
+    plt.figure(fig_num).clf()
+    plt.imshow(image)
+    labels = set()
+    if objs is not None:
+        for o in objs:
+            poly = np.array(o['polygon'])[list(np.arange(len(o['polygon']))) + [0]]
+            plt.plot(poly[:, 0], poly[:, 1], 'r', label=o['label'])
+            labels.add(o['label'])
+        if len(labels) > 1:
+            plt.legend()
+
+
+def test_find_tfl_lights(image_path, json_path=None, fig_num=None):
+    """
+    Run the attention code
+    """
+    image = np.array(Image.open(image_path))
+    if json_path is None:
+        objects = None
+    else:
+        gt_data = json.load(open(json_path))
+        what = ['traffic light']
+        objects = [o for o in gt_data['objects'] if o['label'] in what]
+
+    show_image_and_gt(image, objects, fig_num)
+
+    red_x, red_y, green_x, green_y = find_tfl_lights(image, some_threshold=42)
+    plt.plot(red_x, red_y, 'ro', color='r', markersize=4)
+    plt.plot(green_x, green_y, 'ro', color='g', markersize=4)
+
+
+def main(argv=None):
+    """It's nice to have a standalone tester for the algorithm.
+    Consider looping over some images from here, so you can manually exmine the results
+    Keep this functionality even after you have all system running, because you sometime want to debug/improve a module
+    :param argv: In case you want to programmatically run this"""
+    parser = argparse.ArgumentParser("Test TFL attention mechanism")
+    parser.add_argument('-i', '--image', type=str, help='Path to an image')
+    parser.add_argument("-j", "--json", type=str, help="Path to json GT for comparison")
+    parser.add_argument('-d', '--dir', type=str, help='Directory to scan images in')
+    args = parser.parse_args(argv)
+    default_base = '../../data'
+    if args.dir is None:
+        args.dir = default_base
+    flist = glob.glob(os.path.join(args.dir, '*_leftImg8bit.png'))
+    for image in flist:
+        json_fn = image.replace('_leftImg8bit.png', '_gtFine_polygons.json')
+        if not os.path.exists(json_fn):
+            json_fn = None
+        test_find_tfl_lights(image, json_fn)
+    if len(flist):
+        print("You should now see some images, with the ground truth marked on them. Close all to quit.")
+    else:
+        print("Bad configuration?? Didn't find any picture to show")
+    plt.show(block=True)
+
+
+if __name__ == '__main__':
+    main()
